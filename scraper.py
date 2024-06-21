2352eaf1fc7e1fd0a7467677e753b8806aa52dd4
janmatzek <75334114+janmatzek@users.noreply.github.com>
2024-06-21 19:39:37 +0200

    it scrapes

---
+++ scraper.py
@@ -0,0 +1,116 @@
+"""
+Module to retrieve item prices data from a heureka.cz, a comparison site.
+"""
+
+# TODO: exceptions
+# TODO: async
+# TODO: implement logging (loguru?)
+
+import json
+import os
+import re
+from datetime import datetime
+
+import pandas_gbq
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from google.oauth2 import service_account
+
+from utils import return_list_of_products, return_table_schema, send_response
+
+load_dotenv()
+
+BQ_PROJECT = os.getenv("BQ_PROJECT_ID")
+DESTINATION_DATASET = os.getenv("BQ_DATASET")
+DESTINATION_TABLE = os.getenv("BQ_L0_TABLE_NAME")
+SERVICE_ACCOUNT_PATH = os.getenv("SERVICE_ACCOUNT_PATH")
+
+PRODUCTS = return_list_of_products()
+SCHEMA = return_table_schema()
+
+
+def handler(event, context):
+    """
+    Scrapes backpack prices from heureka and stores them in the DB.
+    """
+    event_body = event.get("body", {}) if "body" in event else {}
+    print("Received event:", json.dumps(event_body))
+
+    print(f"Running {context.function_name}")
+    products_data = pd.DataFrame()
+
+    # this could be async and run faster
+    for product in PRODUCTS:
+
+        response = requests.get(url=product["url"], timeout=10)
+        parsed_html = BeautifulSoup(response.text, "html.parser")
+
+        # fetch last div to bypass "doporučené nabídky"
+        products_list = parsed_html.find_all("div", class_="c-offers-list__cont")[-1]
+
+        data = []
+
+        products = products_list.find_all("section", class_="c-offer")
+
+        current_timestamp = datetime.now()
+
+        for section in products:
+
+            img_tag = section.find("img", class_="c-offer__shop-logo e-image-with-fallback")
+
+            if img_tag:
+                img_alt = img_tag["alt"]
+            else:
+                img_alt = None
+
+            price_tag = section.find("span", class_="c-offer__price u-extra-bold u-delta")
+
+            if price_tag:
+                price = price_tag.text.strip()
+                price = "".join(re.findall(r"\d", price))
+            else:
+                price = None
+
+            data.append(
+                {
+                    "date": current_timestamp,
+                    "product_id": product["product_id"],
+                    "product_name": product["name"],
+                    "color": product["color"],
+                    "shop_name": img_alt,
+                    "price": price,
+                }
+            )
+
+        df_data = pd.DataFrame.from_records(data)
+
+        products_data = pd.concat(objs=[products_data, df_data], axis=0)
+
+    # validate data types
+    for col in SCHEMA:
+        if col["type"] == "STRING":
+            products_data[col["name"]] = products_data[col["name"]].astype(str)
+        elif col["type"] == "FLOAT":
+            products_data[col["name"]] = pd.to_numeric(products_data[col["name"]], errors="coerce")
+        elif col["type"] == "INTEGER":
+            products_data[col["name"]] = pd.to_numeric(
+                products_data[col["name"]], errors="coerce", downcast="integer"
+            )
+        elif col["type"] == "TIMESTAMP":
+            products_data[col["name"]] = pd.to_datetime(products_data[col["name"]], errors="coerce")
+
+    # send the data to BigQuery
+    try:
+        pandas_gbq.to_gbq(
+            dataframe=products_data,
+            project_id=BQ_PROJECT,
+            destination_table=f"{DESTINATION_DATASET}.{DESTINATION_TABLE}",
+            if_exists="append",
+            table_schema=SCHEMA,
+            credentials=service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_PATH),
+        )
+    except Exception as e:  # pylint: disable=broad-except
+        return send_response(500, "Failed to upload the data to BigQuery.", e)
+
+    return send_response(200, f"Retrieved prices of {len(products_data)} products!")
---
+++ utils.py
@@ -0,0 +1,121 @@
+"""Utilities module"""
+
+import json
+import os
+
+import requests
+
+
+def return_table_schema():
+    """Returns table schema."""
+    return [
+        {"name": "date", "type": "TIMESTAMP"},
+        {"name": "product_id", "type": "INTEGER"},
+        {"name": "product_name", "type": "STRING"},
+        {"name": "color", "type": "STRING"},
+        {"name": "shop_name", "type": "STRING"},
+        {"name": "price", "type": "FLOAT"},
+    ]
+
+
+def return_list_of_products():
+    """Returns the list of products where price should be retrieved."""
+    return [
+        {
+            "product_id": 1,
+            "name": "Osprey Aether II 65",
+            "color": "black",
+            "url": "https://turisticke-batohy.heureka.cz/osprey-aether-ii-65l-black_3/#prehled/",
+        },
+        {
+            "product_id": 1,
+            "name": "Osprey Aether II 65",
+            "color": "blue",
+            "url": "https://turisticke-batohy.heureka.cz/osprey-aether-ii-65l-modra_2/#prehled/",
+        },
+        {
+            "product_id": 1,
+            "name": "Osprey Aether II 65",
+            "color": "garlic mustard green",
+            "url": "https://turisticke-batohy.heureka.cz/osprey-aether-ii-65l-garlic-mustard-green_2/#prehled/",
+        },
+        {
+            "product_id": 2,
+            "name": "Osprey Kestrel 58",
+            "color": "black",
+            "url": "https://turisticke-batohy.heureka.cz/osprey-kestrel-58l-black_2/#prehled/",
+        },
+        {
+            "product_id": 2,
+            "name": "Osprey Kestrel 58",
+            "color": "bonsai green",
+            "url": "https://turisticke-batohy.heureka.cz/osprey-kestrel-58l-bonsai-green/#prehled/",
+        },
+        {
+            "product_id": 3,
+            "name": "Osprey Atmos Ag 65",
+            "color": "black",
+            "url": "https://turisticke-batohy.heureka.cz/osprey-atmos-ag-65l-black/#prehled/",
+        },
+        {
+            "product_id": 3,
+            "name": "Osprey Atmos Ag 65",
+            "color": "venturi blue",
+            "url": "https://turisticke-batohy.heureka.cz/osprey-atmos-ag-65l-venturi-blue/#prehled/",
+        },
+        {
+            "product_id": 3,
+            "name": "Osprey Atmos Ag 65",
+            "color": "mythical green",
+            "url": "https://turisticke-batohy.heureka.cz/osprey-atmos-ag-65l-mythical-green/#prehled/",
+        },
+    ]
+
+
+def send_telegram_message(message: str, is_alert: bool):
+    """
+    Function to send a simple message to Telegram via API.
+    The function takes two arguements:
+        1) message (string) - the message that will be sent to Telgram
+        2) is_alert (boolean) - denoting whether the message should be sent
+            to the regular logging channel or the alerting channel
+    The function returns a HTTP response given by the Telegram API
+    """
+    if is_alert:
+        chat_id = os.getenv("TELEGRAM_ALERTING_CHANNEL_ID")
+    else:
+        chat_id = os.getenv("TELEGRAM_LOGGING_CHANNEL_ID")
+
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+
+    response = requests.get(url=url, timeout=10).json()
+
+    return response
+
+
+def send_response(status_code: int, message: str, e=""):
+    """
+    Sends a HTTP response.
+    Args:
+        status_code (int): HTTP status
+        message (str): message in the body of the response
+        e (str) : optional, error description
+    """
+    if e == "":
+        sep = ""
+    else:
+        sep = "\n"
+
+    message = f"{message}{sep}{e}"
+
+    if status_code not in [200, 202]:
+        send_as_alert = False
+    else:
+        send_as_alert = True
+
+    send_telegram_message(message=message, is_alert=send_as_alert)
+
+    response = {"status_code": status_code, "body": json.dumps({"message": f"{message}"})}
+    return response
